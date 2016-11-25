f0c189fc1432de4210df8fa9bfba5d1489367844
thiyagarajan <thiyagarajannv@yahoo.com>
2016-11-25 19:15:52 +0530

    added developer name to project

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -3,4 +3,8 @@ class Developer < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    return "#{first_name} #{last_name}"
+  end
 end
